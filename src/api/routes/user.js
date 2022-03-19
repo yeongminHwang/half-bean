1ecf41a0fb5a01398a4eedf96e8c604ecea66f9c
yeongminHwang <hwongyoungmin@gmail.com>
2022-03-20 00:01:19 +0900

    C:/Program Files/Git/admin/delete -> /admin

--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -13,10 +13,10 @@ module.exports = (app) => {
     try {
       if (req.session.logined) {
         console.log("[+] 로그인 세션이 존재합니다. (로긴되어잇음)");
-        return res.status(200).json({ success: true });
+        return res.status(200).json({ success: true, response: req.session });
       } else {
         console.log("[-] 로그인 세션이 존재하지 않습니다.");
-        return res.status(201).json({ success: false });
+        return res.status(201).json({ success: false, response: req.session });
       }
     } catch (e) {
       console.log("[!]");
@@ -43,15 +43,14 @@ module.exports = (app) => {
         const login_id = req.session.login_id;
         const userInput = req.body.modification;
         //console.log(login_id);
-  
+
         const user = await user_service.updateUser(login_id, userInput);
-  
+
         return res.status(200).json({ success: true, response: user });
       } else {
         console.log("[-] /update :: 로그인 세션이 존재하지 않습니다.");
         return res.status(201).json({ success: false });
       }
-
     } catch (e) {
       res.status(400).json({ success: false, errorMsg: e.message });
     }
@@ -67,7 +66,7 @@ module.exports = (app) => {
         //console.log(login_id);
 
         const isDeleted = await user_service.deleteUser(login_id);
-  
+
         return res
           .status(isDeleted ? 200 : 400)
           .json({ success: isDeleted, response: { isDeleted: isDeleted } });
@@ -75,8 +74,6 @@ module.exports = (app) => {
         console.log("[-] delete :: 로그인 세션이 존재하지 않습니다.");
         return res.status(201).json({ success: false });
       }
-
-
     } catch (e) {
       res.status(400).json({ success: false, errorMsg: e.message });
     }
@@ -84,22 +81,23 @@ module.exports = (app) => {
 
   //===================================================================================
   // 관리자_회원강제 탈퇴
-  router.post("/admin/delete", async (req, res, next) => {
-  //router.delete("/admin", isMaster, async (req, res, next) => {
+  router.delete("/admin", async (req, res, next) => {
+    //router.delete("/admin", isMaster, async (req, res, next) => {
     try {
       if (req.session.admin && req.session.logined) {
         const target_userId = req.body.user_login_id;
 
         const isDeleted = await user_service.deleteUser_admin(target_userId);
-  
+
         return res
           .status(isDeleted ? 200 : 400)
           .json({ success: isDeleted, response: { isDeleted: isDeleted } });
       } else {
-        console.log("[-] 관리자_회원강제 탈퇴 :: 로그인 세션이 존재하지 않습니다.");
+        console.log(
+          "[-] 관리자_회원강제 탈퇴 :: 로그인 세션이 존재하지 않습니다."
+        );
         return res.status(201).json({ success: false });
       }
-
     } catch (e) {
       res.status(400).json({ success: false, errorMsg: e.message });
     }
@@ -113,12 +111,14 @@ module.exports = (app) => {
         console.log(target_userId);
 
         const isRestored = await user_service.reStoreUser_admin(target_userId);
-  
+
         return res
           .status(isRestored ? 200 : 400)
           .json({ success: true, response: { isRestored: isRestored } });
       } else {
-        console.log("[-] 관리자_회원강제 복구 :: 로그인 세션이 존재하지 않습니다.");
+        console.log(
+          "[-] 관리자_회원강제 복구 :: 로그인 세션이 존재하지 않습니다."
+        );
         return res.status(201).json({ success: false });
       }
     } catch (e) {
