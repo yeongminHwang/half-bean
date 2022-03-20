06cdda0997c7c5e779a145ef2af5c14fb9643b96
yeongminHwang <hwongyoungmin@gmail.com>
2022-03-20 16:52:21 +0900

    이메일 인증, 확인, 비밀번호 재설정, 금오웹메일 확인

--- src/api/routes/auth.js
+++ src/api/routes/auth.js
@@ -1,9 +1,12 @@
-const { response } = require("express");
 const { Router } = require("express");
-const { login } = require("../../services/auth");
 const auth_service = require("../../services/auth");
+const user_service = require("../../services/user");
 
 const router = Router();
+
+// Global
+const email_verify_table = {};
+const FINAL_EMAIL_VERIFY_TIME = 1000 * 60 * 10;
 
 module.exports = (app) => {
   // ***/api/auth 로 들어오는 요청 처리 담당
@@ -17,7 +20,7 @@ module.exports = (app) => {
 
       if (!req.session.logined) {
         console.log(req.session.login_id);
-        // 로그인 id, pw 검증 
+        // 로그인 id, pw 검증
         const user = await auth_service.login(login_id, password);
 
         // 세션 저장
@@ -33,10 +36,12 @@ module.exports = (app) => {
         // });
         return res.status(200).json({ success: true, response: user });
       } else {
-        console.log('[-] 로그인이 이미 되어 있는 거 같은데요',req.session.logined);
+        console.log(
+          "[-] 로그인이 이미 되어 있는 거 같은데요",
+          req.session.logined
+        );
         return res.status(200).json({ success: true });
       }
-      
     } catch (e) {
       console.log(e);
       res.status(400).json({ success: false, errorMsg: e.message });
@@ -50,19 +55,17 @@ module.exports = (app) => {
       if (req.session.logined) {
         console.log(req.session.login_id);
         // 세션 삭제 ㄲ
-        req.session.destroy(
-          function (err) {
-            console.log('[+] 세션 삭제 성공');
-            return res.status(200).json({ success: true});
+        req.session.destroy(function (err) {
+          console.log("[+] 세션 삭제 성공");
+          return res.status(200).json({ success: true });
         });
       } else {
-        console.log('[-] 로그인 안되어 있음 ㅇㅅㅇ',req.session.logined);
+        console.log("[-] 로그인 안되어 있음 ㅇㅅㅇ", req.session.logined);
         return res.status(200).json({ success: true });
       }
-      
     } catch (e) {
       console.log(e);
-      res.status(400).json({ success: false});
+      res.status(400).json({ success: false });
     }
   });
 
@@ -74,6 +77,65 @@ module.exports = (app) => {
       const isDouble = await auth_service.doubleCheckId(login_id);
 
       return res.status(200).json({ success: true, response: isDouble });
+    } catch (e) {
+      console.log(e);
+      res.status(400).json({ success: false, errorMsg: e.message });
+    }
+  });
+
+  // 이메일 인증번호 전송
+  router.get("/email", async (req, res, next) => {
+    try {
+      const email = req.body.email;
+
+      const { messageId, random_str } = await auth_service.sendCodebyEmail(
+        email
+      );
+
+      email_verify_table[email] = random_str;
+
+      // 10분뒤 인증코드 만료
+      const timer = setTimeout(() => {
+        delete email_verify_table[email];
+        console.log(email_verify_table);
+      }, FINAL_EMAIL_VERIFY_TIME);
+
+      return res.status(200).json({ success: true, response: { messageId } });
+    } catch (e) {
+      console.log(e);
+      res.status(400).json({ success: false, errorMsg: e.message });
+    }
+  });
+
+  // 이메일 인증번호 확인
+  router.post("/email", async (req, res, next) => {
+    try {
+      const email = req.body.email;
+      const verify_code = req.body.verify_code;
+
+      const isVerify = email_verify_table[email] === verify_code;
+
+      return res.status(isVerify ? 200 : 400).json({
+        success: isVerify,
+        response: { isVerify: isVerify },
+      });
+    } catch (e) {
+      console.log(e);
+      res.status(400).json({ success: false, errorMsg: e.message });
+    }
+  });
+
+  // 비밀번호 재설정
+  router.post("/password/reset", async (req, res, next) => {
+    try {
+      const login_id = req.body.login_id;
+      const changePassword = req.body.changePassword;
+
+      const user = await user_service.updateUser(login_id, {
+        password: changePassword,
+      });
+
+      return res.status(200).json({ success: true, response: user });
     } catch (e) {
       console.log(e);
       res.status(400).json({ success: false, errorMsg: e.message });
