1139d2540c943ae52230dd04dc77c269f6d41ec9
jiravvit <jirabbit05@gmail.com>
2022-04-10 21:30:40 +0900

    session에 데이터 추가
    로그아웃할 때 session destory 어떡하징..

--- src/api/routes/auth.js
+++ src/api/routes/auth.js
@@ -26,20 +26,28 @@ module.exports = (app) => {
       }
 
       // 세션 내 동일 유저 검사 by 로그인 아이디
-      let isLogined = false;
+      req.session.logined = false;
       for (var i = 0; i < req.session.users.length; i++) {
         if (req.session.users[i].login_id === user.dataValues.login_id) {
-          isLogined = true;
+          req.session.logined = true;
           break;
         }
       }
 
       // 로그인 되어있지 않은 유저만 세션에 넣도록
-      if (!isLogined) {
+      if (!req.session.logined) {
         req.session.users.push(user.dataValues);
+
+        // 세션에 값 추가
+        req.session.login_id = user.dataValues.login_id;
+        req.session.user_id = user.dataValues.user_id;
+        req.session.logined = true;
+        if (user.is_master) req.session.admin = true;
+        else req.session.admin = false;  
+      
       }
 
-      console.log(req.session);
+      console.log("지금 로그인한 사람 id", req.session.login_id);
       console.log("현재접속인원 : ", req.session.users.length);
 
       return res.status(200).json({ success: true, response: user });
@@ -50,21 +58,43 @@ module.exports = (app) => {
 
   // 로그아웃
   // 로그아웃 할 때 세션 파괴
-  router.post("/logout", async (req, res, next) => {
+  router.get("/logout", async (req, res, next) => {
     try {
-      const login_id = req.body.login_id;
+      if (req.session.logined) {
+        const login_id = req.session.login_id;
 
-      // 세션에서 로그인 아이디 찾아서 제거
-      for (var i = 0; i < req.session.users.length; i++) {
-        if (req.session.users[i].login_id === login_id) {
-          req.session.users.splice(i, 1);
-          break;
+        // 세션에서 로그인 아이디 찾아서 제거
+        for (var i = 0; i < req.session.users.length; i++) {
+          if (req.session.users[i].login_id === login_id) {
+            req.session.users.splice(i, 1);
+            break;
+          }
         }
+
+        /*
+        세션 삭제하면 req.session.users 이 배열도 함께 삭제됨...
+        사용자 값을 req.session.user 에 넣어주고 req.session.user.destory하면 될 것 같기도한데 이거 고치면 다른 api들 검증 로직 다 고쳐야해서 섣불리 못고치겠음
+        req.session.destroy(function (err) {
+          console.log("[+] 세션 삭제 성공");
+        });
+        */
+
+        // 로그인 상태 false
+        /* 
+         세션 지우면 배열도 날아가서 세션 삭제한 척 함 (로그아웃 된 것처럼 로그인해야 접근할 수 있는 api들 접근 안됨)
+
+         나머지 값들도 지워주면 되는데, 현재 상태가 api에 접속할 때 
+         req.session.logined가 true인 경우를 검증하기 때문에 
+         나머지 값들 안지워줘도 일단은 뭐 괜찮을 거 같음 ㅠㅜ
+        */
+        req.session.logined = false;
+
+        console.log("현재접속인원 : ", req.session.users.length);
+
+        return res.status(200).json({ success: true });
+      } else {
+        throw new Error("로그인 되어 있지 않습니다.");
       }
-
-      console.log("현재접속인원 : ", req.session.users.length);
-
-      return res.status(200).json({ success: true });
     } catch (e) {
       res.status(400).json({ success: false, errorMsg: e.message });
     }
